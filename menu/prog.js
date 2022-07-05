2bf639e25ff8f5a199cbf10d532d6a3e1e33eb2c
Мед Максим <@26kadr.internal>
2022-07-05 12:57:45 +0300

    all files here

---
+++ menu/prog.js
@@ -0,0 +1,13 @@
+log1 = document.getElementById('name');
+pass1 = document.getElementById('password');
+otvet = document.getElementById('ne');
+
+function Check() {
+
+    if (log1.value == 'login' && pass1.value == 'pass') {
+        document.location.href = "sekretnoe.html";
+    }
+    else {
+        otvet.innerHTML = "Пароль";
+    }
+}
